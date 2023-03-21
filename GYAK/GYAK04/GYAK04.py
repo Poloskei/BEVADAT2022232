ad6614242f2bf0ea6b566101d49867ed471d78bf
Palcsek Máté <73887796+Poloskei@users.noreply.github.com>
2023-03-21 13:13:22 +0100

    Update and rename GYAK04.ipynb to GYAK04.py

---
+++ GYAK/GYAK04/GYAK04.py
@@ -0,0 +1,131 @@
+# %%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# %%
+'''
+FONTOS: Az első feladat által visszaadott DataFrame-et kell használni a további feladatokhoz. 
+A függvényeken belül mindig készíts egy másolatot a bemenő df-ről, (new_df = df.copy() és ezzel dolgozz tovább.)
+'''
+
+# %%
+'''
+Készíts egy függvényt ami a bemeneti dictionary-ből egy DataFrame-et ad vissza.
+
+Egy példa a bemenetre: test_dict
+Egy példa a kimenetre: test_df
+return type: pandas.core.frame.DataFrame
+függvény neve: dict_to_dataframe
+'''
+
+
+# %%
+stats = {"country": ["Brazil", "Russia", "India", "China", "South Africa"],
+       "capital": ["Brasilia", "Moscow", "New Dehli", "Beijing", "Pretoria"],
+       "area": [8.516, 17.10, 3.286, 9.597, 1.221],
+       "population": [200.4, 143.5, 1252, 1357, 52.98] }
+def dict_to_dataframe(input_dict):
+    df = pd.DataFrame(input_dict)
+    return df
+
+test_df = dict_to_dataframe(stats)
+
+# %%
+'''
+Készíts egy függvényt ami a bemeneti DataFrame-ből vissza adja csak azt az oszlopot amelynek a neve a bemeneti string-el megegyező.
+
+Egy példa a bemenetre: test_df, 'area'
+Egy példa a kimenetre: test_df
+return type: pandas.core.series.Series
+függvény neve: get_column
+'''
+
+# %%
+def get_column(input_df, input_string)->pd.core.series.Series:
+    new_df = input_df.copy()
+    return new_df[input_string]
+
+get_column(test_df,'area')
+
+
+# %%
+'''
+Készíts egy függvényt ami a bemeneti DataFrame-ből vissza adja a két legnagyobb területű országhoz tartozó sorokat.
+
+Egy példa a bemenetre: test_df
+Egy példa a kimenetre: test_df
+return type: pandas.core.frame.DataFrame
+függvény neve: get_top_two
+'''
+
+# %%
+def get_top_two(input_df)->pd.core.frame.DataFrame:
+    new_df = input_df.copy()
+    return new_df.nlargest(2,'area')
+
+print(get_top_two(test_df))
+
+# %%
+'''
+Készíts egy függvényt ami a bemeneti DataFrame-ből kiszámolja az országok népsűrűségét és eltárolja az eredményt egy új oszlopba ('density').
+(density = population / area)
+
+Egy példa a bemenetre: test_df
+Egy példa a kimenetre: test_df
+return type: pandas.core.frame.DataFrame
+függvény neve: population_density
+'''
+
+# %%
+def population_density(input_df)->pd.core.frame.DataFrame:
+    new_df = input_df.copy()
+    density = np.round(new_df['population'] / new_df['area'])
+    new_df['density'] = density
+    return new_df
+
+print(population_density(test_df))
+
+# %%
+'''
+Készíts egy függvényt, ami a bemeneti Dataframe adatai alapján elkészít egy olyan oszlopdiagramot (bar plot),
+ami vizualizálja az országok népességét.
+
+Az oszlopdiagram címe legyen: 'Population of Countries'
+Az x tengely címe legyen: 'Country'
+Az y tengely címe legyen: 'Population (millions)'
+
+Egy példa a bemenetre: test_df
+Egy példa a kimenetre: fig
+return type: matplotlib.figure.Figure
+függvény neve: plot_population
+'''
+
+# %%
+def plot_population(input_df)->plt.figure.Figure:
+    new_df = input_df.copy
+    fig = plt.figure(figsize = (10, 5))
+    fig.bar(new_df['country'],new_df['population'], color ='maroon', width = 0.4)
+    
+    fig.set_xlabel('Country')
+    fig.set_ylabel('Population')
+    fig.set_title('Population of Countries')
+    return fig
+
+# %%
+'''
+Készíts egy függvényt, ami a bemeneti Dataframe adatai alapján elkészít egy olyan kördiagramot,
+ami vizualizálja az országok területét. Minden körcikknek legyen egy címe, ami az ország neve.
+
+Az kördiagram címe legyen: 'Area of Countries'
+
+Egy példa a bemenetre: test_df
+Egy példa a kimenetre: fig
+return type: matplotlib.figure.Figure
+függvény neve: plot_area
+'''
+
+# %%
+
+
+
