55b1d022ddc958bf50610b2c1b9c288b178058af
Palcsek Máté <73887796+Poloskei@users.noreply.github.com>
2023-04-01 23:50:58 +0200

    Add files via upload

--- HAZI/HAZI05/HAZI05.py
+++ HAZI/HAZI05/HAZI05.py
@@ -7,10 +7,10 @@ from sklearn.metrics import confusion_matrix
 class KNNClassifier:
     @staticmethod
     def load_csv(csv_path:str) ->Tuple[pd.DataFrame,pd.DataFrame]:
-        #np.random.seed(42)
-        df = pd.read_csv(csv_path,delimiter=',')
-        df.sample(frac=1,random_state=42).reset_index(drop=True)
+        df = pd.read_csv(csv_path,na_values='""',delimiter=',',header=None)
+        df = df.sample(frac=1,random_state=42).reset_index(drop=True)
         x,y = df.iloc[:,:4],df.iloc[:,-1]
+        y = pd.DataFrame(y).reset_index(drop=True)
         return x,y
 
 
@@ -18,10 +18,14 @@ class KNNClassifier:
         self.k = k
         self.test_split_ratio = test_split_ratio
         
-
-    @classmethod
+    @property
     def k_neighbors(self)->int:
         return self.k
+    
+    @k_neighbors.setter
+    def k_neighbors(self,value):
+        self.k=value
+    
 
     def train_test_split(self,features:pd.DataFrame,labels:pd.DataFrame) -> None:
         test_size = int(len(features) * self.test_split_ratio)
@@ -36,23 +40,44 @@ class KNNClassifier:
         self.x_test = x_test
         self.y_test = y_test
 
+        
 
     def euclidean(self,element_of_x:pd.DataFrame) -> pd.DataFrame:
-        return ((self.x_train - element_of_x).sum()**2).pow(1./2)
+        return pd.DataFrame(((self.x_train - element_of_x)**2).sum().pow(1./2))
 
-    def predict(self,x_test:np.ndarray) -> np.ndarray:
+
+    def predict(self,x_test:pd.DataFrame):
         labels_pred = []
-        for x_test_element in x_test:
-            distances = self.euclidean(x_test_element)
-            distances = np.array(sorted(zip(distances,self.y_train)))
-            label_pred = mode(distances[:self.k,1],keepdims=False).mode
+        for idx,x_test_element in x_test.iterrows():
+            distances = self.euclidean(self.x_train,x_test_element)
+            distances['y']=self.y_train
+            distances.sort_values(by=[0],inplace=True)
+            label_pred = distances.head(self.k)['y'].mode()
+            #print(label_pred)
             labels_pred.append(label_pred)
-        self.y_preds = np.array(labels_pred,dtype=np.int32)
+        self.y_preds = pd.DataFrame(labels_pred)
 
     def accuracy(self) -> float:
+        self.y_test.reset_index(drop=True,inplace=True)
+        self.y_preds.reset_index(drop=True,inplace=True)
+        self.y_test.columns=['y']
+        self.y_preds.columns=['y']
         true_positive = (self.y_test == self.y_preds).sum()
         return true_positive / len(self.y_test) * 100
 
     def confusion_matrix(self):
         conf_matrix = confusion_matrix(self.y_test,self.y_preds)
         return conf_matrix
+    
+    def best_k(self)->Tuple[int,float]:
+        bestk=0
+        bestval=0.0
+        for i in range(1,20):
+            k=i
+            self.predict(self,self.x_test)
+            acc = self.accuracy(self)
+            if acc > bestval:
+                bestval=acc
+                bestk=i
+        return (bestk,bestval)
+
